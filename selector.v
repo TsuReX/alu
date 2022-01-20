dd72b6d6f3defbef78f2309aad7ce59069869e28
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2022-01-20 17:29:25 +0300

    selector module body was added, project is compiled.

---
+++ selector.v
@@ -0,0 +1,17 @@
+module selector(inc_a, dec_a, inc_b, dec_b, inc_f, dec_f, sel, inc_pulse, dec_pulse, prev_pusle, next_pulse, clock);
+
+output inc_a;
+output dec_a;
+output inc_b;
+output dec_b;
+output inc_f;
+output dec_f;
+output sel;
+
+input inc_pulse;
+input dec_pulse;
+input prev_pusle;
+input next_pulse;
+input clock;
+
+endmodule
