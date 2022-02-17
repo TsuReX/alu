e6e6360df1b375fc12cba36bdd626535228e1ea5
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2022-02-17 18:14:00 +0300

    traffic_light module was finished, but wasn't tested, project is compiled.

--- traffic_light.v
+++ traffic_light.v
@@ -7,33 +7,47 @@ localparam BLG_STATE = 3'b011;
 localparam Y_STATE = 3'b100;
 
 localparam STOP_VAL = 8'hA;
+localparam BLG_STOP_VALUE = 8'h2;
 
 reg state;
 reg [7:0] counter;
+reg [7:0] blg_counter;
 
-always @(posedge reset, posedge clock) begin
+always @(posedge reset, posedge clock) begin	
 	
 	if (reset == 1'b1) begin
 		state <= R_STATE;
 		counter <= 8'b0;
+		blg_counter <= 1;
 		{red, yellow, green} <= 3'b100;
-		
 	end else begin
 		
 		case (state)
-			R_STATE: begin
-			
+			R_STATE: begin				
+				if (counter == STOP_VAL) begin
+					counter <= 1;
+					state <= RY_STATE;
+					{red, yellow, green} <= 3'b110;
+				end else begin
+					counter <= counter + 1;
+				end
 			end
 			
 			RY_STATE: begin
-			
+				if (counter == STOP_VAL) begin
+					counter <= 1;
+					state <= G_STATE;
+					{red, yellow, green} <= 3'b001;
+				end else begin
+					counter <= counter + 1;
+				end	
 			end
 			
 			G_STATE: begin
 				if (counter == STOP_VAL) begin
 					counter <= 1;
 					state <= BLG_STATE;
-					green <= 0;
+					{red, yellow, green} <= 3'b000;
 				end else begin
 					counter <= counter + 1;
 				end
@@ -42,22 +56,34 @@ always @(posedge reset, posedge clock) begin
 			BLG_STATE: begin
 				if (counter == STOP_VAL) begin
 					counter <= 1;
-					state <= BLG_STATE;
-					green <= 0;
-					red <= 1;
+					blg_counter <= 1;
+					state <= Y_STATE;
+					{red, yellow, green} <= 3'b010;
 				end else begin
 					counter <= counter + 1;
-					green <= ~green;
+					
+					if (blg_counter == BLG_STOP_VALUE) begin
+						blg_counter <= 1;
+						green <= ~green;
+					end else begin
+						blg_counter <= blg_counter + 1;
+					end	
 				end
 			end
 			
 			Y_STATE: begin
-			
+				if (counter == STOP_VAL) begin
+					counter <= 1;
+					state <= R_STATE;
+				end else begin
+					counter <= counter + 1;
+				end
 			end
 			
 			default: begin
 				state <= R_STATE;
 				counter <= 8'b0;
+				{red, yellow, green} <= 3'b100;
 			end
 		endcase
 	
