204df49d82b9b115972ba7853ea1d8929c0dbe3d
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2022-03-04 15:06:41 +0300

    New implementation of organizer module was realized.

--- organizer.v
+++ organizer.v
@@ -1,42 +1,53 @@
-module organizer(output reg [4:0] A, output reg [4:0] B, output reg [4:0] func, output reg [1:0] sel, input dec, input inc, input prev, input next);
+module organizer(A, B, func, sel, clock, dec, inc, prev, next);
+
+output reg [4:0] A;
+output reg [4:0] B;
+output reg [4:0] func;
+output reg [1:0] sel;
+
+input clock;
+input dec;
+input inc;
+input prev;
+input next;
 
 localparam SEL_A = 2'b00;
 localparam SEL_B = 2'b01;
 localparam SEL_F = 2'b10;
 
 
-always @ (posedge prev or posedge next) begin
+always @ (posedge clock) begin
 	
 	if (prev == 1)
-		sel <= sel - 1;
-	else if (next == 1)
-		sel <= sel + 1;	
+		sel <= sel - 2'b1;
+	if (next == 1)
+		sel <= sel + 2'b1;	
 
 end
 
-always @ (posedge dec or posedge inc) begin
+always @ (posedge clock) begin
 
 	case (sel)
 
 		SEL_A: begin
 			if (dec == 1)
 				A <= A - 1;
-			else if (inc == 1)
+			if (inc == 1)
 				A <= A + 1;
 		end
-		
+	
 		SEL_B: begin
 			if (dec == 1)
 				B <= B - 1;
-			else if (inc == 1)
+			if (inc == 1)
 				B <= B + 1;
 		end
 		
 		default: begin
 			if (dec == 1)
-				func <= func - 1;
-			else if (inc == 1)
-				func <= func + 1;
+				func <= func - 2'b1;
+			if (inc == 1)
+				func <= func + 2'b1;
 		end
 
 	endcase
